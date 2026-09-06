1e42894ecc39d94b2d98c58821d6f70bb60cba85
ayush <axlee484@users.noreply.github.com>
2026-09-06 13:08:47 +0530

    add coin prefab

---
+++ Assets/Prefabs/Coin/Coin.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private float value;
+   
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+}
