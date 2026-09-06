f1bc2707b0d6c502d86ef84fef1320d58c23804a
ayush <axlee484@users.noreply.github.com>
2026-09-06 14:05:49 +0530

    update pickup

--- Assets/Components/Pickable/PickUp.cs
+++ Assets/Components/Pickable/PickUp.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 
-public  class Pickable : MonoBehaviour
+public  class PickUp : MonoBehaviour
 {
     public virtual void DestroySelf()
     {
         Destroy(gameObject);
     }
-    public virtual void PickUp(Collider2D otherCollider)
+    public virtual void OnPickUp(Collider2D otherCollider)
     {
         DestroySelf();
     }
     private void OnTriggerEnter2D(Collider2D otherCollider)
     {
-        PickUp(otherCollider);
+        OnPickUp(otherCollider);
     }
 }
