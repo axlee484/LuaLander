352a6dd11016e9543905a168d68ef58fb9c2b62e
ayush <axlee484@users.noreply.github.com>
2026-09-06 12:52:32 +0530

    add game manager singleton

---
+++ Assets/GameAssets/Singletons/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    
+}
