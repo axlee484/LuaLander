b458c13da7131da89a1e617d64a943b3f2a52b7c
ayush <axlee484@users.noreply.github.com>
2026-09-07 17:38:50 +0530

    fix stats ui

---
+++ Assets/Prefabs/UI/StatsUIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+enum STAT_TYPE
+{
+    SCORE,
+    TIME,
+    
+};
+
+public class StatsUIManager : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
